f35d520a854497a6dabb35b3fa55169436f9a856
Adefolajuwon <oadefolajuwon@gmail.com>
2024-10-27 01:14:26 +0100

    feature: Added brown

--- src/app/components/Cell.js
+++ src/app/components/Cell.js
@@ -1,7 +1,8 @@
 const Cell = ({ isFilled, onClick }) => {
     return (
       <div
-        className={`w-8 h-8 cursor-pointer border ${isFilled ? 'bg-green-600' : 'bg-blue-500'}`} // Tailwind styles
+      className={`w-8 h-8 cursor-pointer border 
+        ${isFilled ? (Math.random() > 0.5 ? 'bg-green-700' : 'bg-amber-950') : 'bg-blue-600'}`}
         onClick={onClick}
       />
     );
